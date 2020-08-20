a0baee95a5166bc86dd83b4dfceb3b4af8c1a165
shirleyndou <shirley.ndou@umuzi.org>
2020-08-20 10:06:40 +0200

    Bank account class with Deposit, Withdraw methods

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -1,3 +1,42 @@
 public class BankAccount {
+    private int idNumber;
+    private String name, surname;
 
+    int balance=0;
+    int lastTransaction;
+
+    public BankAccount(int id, String name, String surname){
+        this.idNumber = id;
+        this.name=name;
+        this.surname=surname;
+    }
+
+    public void setDeposit(int deposited){
+        if(deposited !=0){
+            balance += deposited;
+            lastTransaction = deposited;
+        }
+    }
+
+    public void setWithdraw(int withdraw){
+        if(withdraw !=0){
+            balance -= withdraw;
+            lastTransaction= -withdraw;
+        }
+    }
+
+    public void getLastTransaction(){
+        if(lastTransaction > 0){
+            System.out.println("Deposited " + lastTransaction);
+        }else if(lastTransaction <0){
+            System.out.println("Withdrew " + Math.abs(lastTransaction)); //abs- so it returns a positive value
+        }else{
+            System.out.println("No transaction occurred");
+        }
+    }
+
+    public String toString(){
+
+        return "Account holder ID: " + this.idNumber + "\nAccount holder name: " + this.name + "\nAccount holder surname: " +this.surname;
+    }
 }
