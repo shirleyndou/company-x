0cea6294bc852a126a3136e75415d3aaa9aa4694
shirleyndou <shirley.ndou@umuzi.org>
2020-08-20 10:00:34 +0200

    Company X

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+       /* BankAccount bankAccount = new BankAccount(1, "Shirley", "Ndou");
+        System.out.println(bankAccount.toString());
+       // System.out.println("Current balance = " + bankAccount.balance);
+        bankAccount.setDeposit(500);
+        bankAccount.getLastTransaction();
+        bankAccount.setWithdraw(50);
+        bankAccount.getLastTransaction();
+        System.out.println("Current balance = " + bankAccount.balance);*/
+    }
+}
